1f16d08245d0a72ffd6687da9ab08f4f92f38e8c
Dean4132 <olalekanbamigboye732@gmail.com>
2026-01-16 10:27:31 +0100

    Create intro.js

---
+++ intro.js
@@ -0,0 +1,9 @@
+const myName = "Bamigboye Olalekan";
+const age = 24;
+const favoriteProgrammingLanguage = "Javascript";
+
+console.log(myName);
+console.log(age);
+console.log(favoriteProgrammingLanguage);
+
+console.log("my name is " + myName + ", i am " + age + " years old" + " and my favorite programming language is " + favoriteProgrammingLanguage )
